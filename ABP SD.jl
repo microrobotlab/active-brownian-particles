60de65fa08cf63f5c268f5e91e6b8a1b88f5cf73
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2023-07-31 16:37:02 +0200

    updating SD for plots

--- ABP SD.jl
+++ ABP SD.jl
@@ -5,9 +5,9 @@
 using Plots, LaTeXStrings, Statistics, CSV, DataFrames
 
 gr()
-
+pathf = "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\July\\parameter scan\\20230729-181916\\R=2.0 v=1.0 a=50.0 b=25.0 pf=0.1\\run2\\"
 function stat_analysis2(a,b,R,pathf)
-  f1= pathf*"_p.csv"
+  f1= pathf*"20230729-181916 R=2.0 v=1.0 a=50.0 b=25.0 pf=0.1 run2_p.csv"
   df= CSV.read(f1, DataFrame)
   neq= df[!,:p1]
   npole= df[!,:p2]
@@ -26,10 +26,17 @@ Ap= a*b*(atan(b/(tan(eθ)*a)))
   sdpfe= sdeq*(0.5*π/Aeq)*pf_factor  # standard deviation of pf at equators
   sdpfp= sdpole*(0.5*π/Ap)*pf_factor 
 
-plot( [1:2], [mpfe,mpfp], yerror = [sdpfe,sdpfp], xtick=0:3, ytick= 0:0.2:0.5, xlimit= (0,3), ylimit= (0,0.5), grid= false, legend = false, ytickfont=font(17), xtickfont=font(17), linewidth=2, linecolor= "black")
+#plot( [1:2], [mpfe,mpfp], yerror = [sdpfe,sdpfp], xtick=0:3, ytick= 0:0.2:0.5, xlimit= (0,3), ylimit= (0,0.5), grid= false, legend = false, ytickfont=font(17), xtickfont=font(17), linewidth=2, linecolor= "black")
 
-savefig(pathf*"_error.png")
+#savefig(pathf*"_error.png")
 return mpfe, sdpfe,mpfp, sdpfp
 #return meq, sdeq, mpole,sdpole
 
 end
+
+L = 100.0 	# μm box length
+R = 2.0		# μm particle radius
+
+a=L/2
+b=L/4
+out= stat_analysis2(a,b,R,pathf)
