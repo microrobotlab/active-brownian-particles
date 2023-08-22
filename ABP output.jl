8aa3af85e74cc487fb0a2894aba4c55f3348a6ef
Jyoti Sharma <115004180+spm-sudo-echo@users.noreply.github.com>
2023-08-22 14:27:45 +0200

    ploting sqaure

--- ABP output.jl
+++ ABP output.jl
@@ -30,7 +30,9 @@ ICS=1         # number of intial conditons to be scanned
 #pf_factor = (R^2)/(a*b)
 pf_factor = (R^2)
 DT, DR = diffusion_coeff(R).*[1e12, 1]
-packing_fraction = 0.1
+packing_fraction = 0.4
+
+
 Np = round(Int,packing_fraction*L^2/(2R^2))  #Np is the number of particles in my set and I choose it random?
 #π
 Nt = 100000# Nt is the number of steps 
@@ -117,8 +119,6 @@ end
 anim = @animate for i = 1:100:Nt
     scatter(graph_wall[1][i][:,1], graph_wall[1][i][:,2], aspect_ratio=:equal, lims=(-L/2, L/2),markersize=350R/L,marker =:circle,legend=false, title = "$(inside_Np) particles, steps $i, ellipse a=L/2, b= L/4")
     #plot!(L/2*cos.(-π:0.01:π), L/4*sin.(-π:0.01:π)) # ellipse
-    plot!([L/2], seriestype="vline")  #sqaure
-    plot!([-L/2], seriestype="vline")
     quiver!(graph_wall[1][i][:,1],graph_wall[1][i][:,2],quiver=(4*cos.(graph_wall[2][i,1]),4*sin.(graph_wall[2][i,1])), color=:red)
 end
 #marker_z=graph_wall[2][i,1], color=:rainbow, for 
