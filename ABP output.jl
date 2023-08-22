7f90e306d365d5bc4a9480ba8c268c914cf3e37c
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2023-08-22 17:41:01 +0200

    working on averaging for transient analysis

--- ABP output.jl
+++ ABP output.jl
@@ -26,19 +26,19 @@ R = 2.0		# μm particle radius
 v = 10.0 	# μm/s particle velocity
 a=L/2
 b=L/4
-ICS=1         # number of intial conditons to be scanned 
+ICS=2         # number of intial conditons to be scanned 
 #pf_factor = (R^2)/(a*b)
 pf_factor = (R^2)
 DT, DR = diffusion_coeff(R).*[1e12, 1]
 packing_fraction = 0.1
 Np = round(Int,packing_fraction*L^2/(2R^2))  #Np is the number of particles in my set and I choose it random?
 #π
-Nt = 100000# Nt is the number of steps 
+Nt = 1000# Nt is the number of steps 
 #println(" Number of particles: $Np") 
 #-------------------------------------------------------------------------------------------------------------------
 
 # destination folders selection
-path="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\08.Aug\\square\\" # destination folder path
+path="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\08.Aug\\ellipse\\" # destination folder path
 
 datestamp=Dates.format(now(),"YYYYmmdd-HHMMSS")  # todays date
 
@@ -66,7 +66,7 @@ for i=1:ICS
 # THIS IS THE CODE TO CALL WALL FUNCTIONS IN THE MAIN FUNCTION for square wall condition
 
 # Same with the wall condition (particles bounce off the edge)  
-graph_wall = multiparticleE_wall(Np,L,R,v,Nt) 
+#graph_wall = multiparticleE_wall(Np,L,R,v,Nt) 
 
 #=
 scatter(graph[1][:,1], graph[1][:,2], markersize=350R/L, legend=false, aspect_ratio=:equal, title = "$Np particles, step n°1")
@@ -82,7 +82,7 @@ plot!([-L/2], seriestype="vline")
 
 
 
-#graph_wall = multiparticleE_wall(Np,L,R,v,Nt) # has values of x and y posiiton in each frame in graph_wall[1]
+graph_wall = multiparticleE_wall(Np,L,R,v,Nt) # has values of x and y posiiton in each frame in graph_wall[1]
 
 println("multiparticleE_wall complied\n")
 #---------------------------------------------------------------------------------------------------------------------
@@ -90,7 +90,7 @@ println("multiparticleE_wall complied\n")
 #file_store(graph_wall,Nt,pathf)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 # analysis
-#inside_Np=stat_analysis1(a,b,R,pathf)
+inside_Np=stat_analysis1(a,b,R,pathf)
 # mean and standard deviation
 
 #analysis_SD= stat_analysis2(a,b,R,pathf)
@@ -99,6 +99,7 @@ println("multiparticleE_wall complied\n")
 
 
 #------------------------------------------------------------------------------For square-------------------------------------------------------------------------
+#=
 anim = @animate for i = 1:100:Nt
     scatter(graph_wall[1][i][:,1], graph_wall[1][i][:,2], aspect_ratio=:equal, lims=(-L/2, L/2),markersize=350R/L,marker =:circle,legend=false, title = "$(Np) particles, steps $i, ")
     
@@ -111,14 +112,12 @@ end
 f1= pathf*".gif"
 gif(anim, f1)
 end
-
+=#
 #------------------------------------------------------------------------------for ellipse-------------------------------------------------------------------------
-#=
+
 anim = @animate for i = 1:100:Nt
     scatter(graph_wall[1][i][:,1], graph_wall[1][i][:,2], aspect_ratio=:equal, lims=(-L/2, L/2),markersize=350R/L,marker =:circle,legend=false, title = "$(inside_Np) particles, steps $i, ellipse a=L/2, b= L/4")
-    #plot!(L/2*cos.(-π:0.01:π), L/4*sin.(-π:0.01:π)) # ellipse
-    plot!([L/2], seriestype="vline")  #sqaure
-    plot!([-L/2], seriestype="vline")
+    plot!(L/2*cos.(-π:0.01:π), L/4*sin.(-π:0.01:π)) # ellipse
     quiver!(graph_wall[1][i][:,1],graph_wall[1][i][:,2],quiver=(4*cos.(graph_wall[2][i,1]),4*sin.(graph_wall[2][i,1])), color=:red)
 end
 #marker_z=graph_wall[2][i,1], color=:rainbow, for 
@@ -127,6 +126,6 @@ f1= pathf*".gif"
 gif(anim, f1)
 
 end
-=#
+
 
 
