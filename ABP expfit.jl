dbe68c2bb0367b4479194bbd5842402a9b18d1e9
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2023-08-31 15:51:33 +0200

    updating fitting

--- ABP expfit.jl
+++ ABP expfit.jl
@@ -4,26 +4,38 @@
 
 # m(t,p)=p1*exp(−p2*t)
 
-using LsqFit, Plots, DataFrames, CSV
+using LsqFit, Plots, DataFrames, CSV, CurveFit
 
 path = "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\08.Aug\\ellipse\\20230824-205011\\R=2.0 v=10.0 a=50.0 b=25.0 pf=0.1\\"
 f= path*"average100.csv"
 df= CSV.read(f,DataFrame)
-model(t, p) = p[1] * exp.(-p[2] * t)
 
-tdata = df[!,:t]./100.0
-neq= df[!,:e]
-np= df[!,:p]
+# define model
+y(t) = a * exp(b * t)
+model(t, p) = p[1] * exp.(p[2] * t)   # exponential fitting
+
+# linear fitting
+
+#model(t, p) = p[1] + (p[2] * t)  
+
+tdata = 0:0.1:2.0
+ydata = 3.0 * exp.(-2.0 * tdata) + 0.1*randn(length(tdata))
+
+
+#tdata = df[100:1000,:t]./100.0
+#neq= df[100:1000,:e]
+#np= df[100:1000,:p]
+#tdata= log10.(tdata)
 #ydata = model(tdata, [1.0 2.0]) + 0.01*randn(length(tdata))
-ydata=neq
-p0 = [0.5, 10.0]
+#ydata= log10.(ydata)
+#ydata= neq
+#ydata=log10.(neq)
+p0 = [2.0, 1.5]
 
 fit = curve_fit(model, tdata, ydata, p0)
 param = fit.param
 yfit= model(tdata, param)
 
-
-
 p=plot(tdata,ydata, seriestype=:scatter, label="Data")
-q= plot!(tdata,yfit,label="Fitted $((param[1]))exp(-$(param[2])t)")
+q= plot!(tdata,yfit,label="Fitted $((param[1]))exp($(param[2])t)")
 display(q)
