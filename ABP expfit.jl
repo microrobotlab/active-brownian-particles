97a660ded362358d04eec42a1d1920581333d619
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2023-08-31 16:47:05 +0200

    FFT updates

--- ABP expfit.jl
+++ ABP expfit.jl
@@ -20,27 +20,30 @@ model(y, p) = p[1] .+ (p[2] * y)         # linear fitting
 
 #model(t, p) = p[1] + (p[2] * t)  
 
-tdata = df[5000:10000,:t]./100.0
-neq= df[5000:10000,:e]
-np= df[5000:10000,:p]
+tdata = df[9250:10000,:t]./100.0
+neq= df[9250:10000,:e]
+np= df[9250:10000,:p]
 ydata= neq
 jdata = 0.0026 * exp.(0.471 * neq) ##+ 0.1*randn(length(neq))
 #tdata= log10.(tdata)
 #ydata = model(tdata, [1.0 2.0]) + 0.01*randn(length(tdata))
 #ydata= log10.(ydata)
-
+plot(tdata,ydata, seriestype=:scatter, label="Data")
 #ydata=log10.(neq)
 p0 = [25.0, 0.01]
+fs=1.0
+
+freq= fftshift(fft(neq))
+freqs = fftshift(fftfreq(length(tdata), fs))
+
+k= plot(freqs,abs.(freq), xlimit=(-0.02,0.02), seriestype=:scatter)
 
 
-freq= fft(neq)
-
-
-k= plot(abs.(freq))
 
 display(k)
 
 
+#=
 fit = curve_fit(model, ydata, tdata, p0)
 param = fit.param
 xfit= model(ydata, param)
@@ -50,3 +53,4 @@ p=plot(tdata,ydata, seriestype=:scatter, label="Data")
 q= plot!(xfit,ydata,label="Fitted $(param[1])t+ $(param[1])")
 display(q)
 #savefig(q,f1)
+=#
