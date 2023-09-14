93c4fc248a962b6a58de259e5f81291348afb5c8
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2023-09-14 17:37:54 +0200

    testing vcat for dataframes

--- ABP runavg.jl
+++ ABP runavg.jl
@@ -1,19 +1,24 @@
 # code can take files from multiple folders and can calculate their running average (select window_size) and FFT
 
 using DataFrames, CSV, Statistics,  FFTW, Plots
+path1 = "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\08.Aug\\ellipse\\20230824-205011\\R=2.0 v=10.0 a=50.0 b=25.0 pf=0.1\\"
 
-for i in 1:1
+all_data= DataFrame( run=Int64[],f=Float64[], real= Float64[])
+
+
+ # creates an empaty data frame
+
+for i in 1:2
 
 path = "C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2023\\08.Aug\\ellipse\\20230824-205011\\R=2.0 v=10.0 a=50.0 b=25.0 pf=0.1\\run$i\\"
-path1= "..\\"
+
 filename= "20230824-205011 R=2.0 v=10.0 a=50.0 b=25.0 pf=0.1 run$(i)_p"
 pathf= path*filename
 f= pathf*".csv"
 f1= path*"running_avg_FFT_run$i.png"
-f2=path*"FFT_data.csv"
+f2=path1*"FFT_data.csv"
 
 df= CSV.read(f,DataFrame) # e is equators particles , p is poles particles
-
 
 window_size= 10
 
@@ -30,13 +35,20 @@ end_frame= 10000
 fs=1.0
 freq= fftshift(fft(df[start_frame:end_frame,:running_avg]))
 freqs = fftshift(fftfreq(length(df[start_frame:end_frame,:running_avg]), fs))
+check=i
+check= DataFrame( run=i,f=freqs, real= real.(freq)) 
+testing=vcat(all_data,check)
+show(testing)
+#all_data= vcat(all_data,data)
 
-k= plot(freqs,abs.(freq), xlimit=(-0.3,0.3), ylimit=(0.02,1000),seriestype=:stem)
+
+#CSV.write(f2,df1)
+k= plot(freqs,real.(freq), xlimit=(-0.3,0.3), ylimit=(0.02,2000),seriestype=:stem)
 
 display(k)
-savefig(k,f1)
-
+#savefig(k,f1)
 end
+
 #y=plot(df_avg[start_frame:end_frame,:t]./100.0,df_avg[start_frame:end_frame,:running_avg], seriestype= :scatter, xlimit=(start_frame,end_frame))
 
 
