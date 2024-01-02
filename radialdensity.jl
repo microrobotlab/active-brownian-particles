09aaeb10a8b8521054d2204f7395b1a3890559b4
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-01-02 16:28:22 +0100

    are them still useful?

--- radialdensity.jl
+++ radialdensity.jl
@@ -3,7 +3,40 @@ using LinearAlgebra
 using Plots
 using Random
 
-function radial_density_sq(xy::Array{Float64,2}, nshells::Int64, L::Float64)
+function intersection_area(a,b,R₁,R₂) #a larger semiaxis, b smaller semiaxis, R₁ smaller circle radius
+
+    if R₂ <= b
+        area = pi*(R₂^2-R₁^2)
+    end
+
+    if R₁ < b && R₂ > b
+        area = ellipse_sector(a,b,R₂)-(pi*R₁^2)
+    end
+
+    if R₁ >= b
+        area = ellipse_sector(a,b,R₂) - ellipse_sector(a,b,R₁)
+    end
+
+    return area
+end
+
+
+function ellipse_sector(a,b,r) # area of intersection between ellipse and circle with r >b
+    x = a*sqrt((r^2-b^2)/(a^2-b^2))
+    y = b*sqrt((a^2-r^2)/(a^2-b^2))
+
+    θ₁ = atan(y,x)
+    θ₂ = atan(y,-x)
+    Aₑ = 0.5a*b*atan(a*tan(θ₁)/b)
+    Aᵢₙₜ = pi*a*b - 4*Aₑ
+    # Aₑ = a*b*(θ₂-θ₁-atan((b-a)*sin(2θ₂)/(a+b+(b-a)*cos(2*θ₂)))+atan((b-a)*sin(2θ₁)/(a+b+(b-a)*cos(2*θ₁))))
+
+    Aₒ = 2*θ₁*r^2
+
+    return Aᵢₙₜ+Aₒ
+end
+
+function radial_density_sqsq(xy::Array{Float64,2}, nshells::Int64, L::Float64) #square norm!
     thickness = .5*L/nshells
     lims = [i*thickness for i in 0:nshells-1]
     nums = zeros(nshells)
@@ -19,55 +52,83 @@ function radial_density_sq(xy::Array{Float64,2}, nshells::Int64, L::Float64)
 end
 
 function radial_density_cr(xy::Array{Float64,2}, nshells::Int64, R::Float64)
-    thickness = 0.5R/nshells
+    thickness = R/nshells
+    add(x) = x+thickness
     lims = [i*thickness for i in 0:nshells-1]
+    lims = hcat(lims, map(add, lims))
 
     r = (xy[:,1]).*(xy[:,1]) + (xy[:,2]).*(xy[:,2])
-  
     rₚ = sqrt.(r)   
-    # α =atan.(xy[:,2], xy[:,1]) 
     nums = zeros(nshells)
 
-    for i in 1:nshells
-        id = rₚ .> lims[i]
-        nums[i] = sum(id)
+    for row in eachrow(lims)
+        r1 = row[1]
+        r2 = row[2]
+    
+        idx = (rₚ .>= r1  .&& rₚ.<= r2)
+
+        nums[i] = sum(idx)
+        areas[i] = intersection_area(a,b,r1,r2)
+        i+=1 # smarter way to do that?
     end
-    area = π*((lims.+thickness).^2 .- lims.^2)
-    nc = [nums[i]-nums[i+1] for i in 1:length(nums)-1]
-    nc = vcat(nc, nums[end])
+    area = π*(lims[:,2].^2 .- lims[:,1].^2)
 
     return nc./area
 end
 
-function radial_density_el(xy::Array{Float64,2}, nshells::Int64, L::Float64)
+function radial_density_el(xy::Array{Float64,2}, nshells::Int64, a::Float64, b::Float64)
     r = (xy[:,1]).*(xy[:,1]) + (xy[:,2]).*(xy[:,2])
     rₚ = sqrt.(r)   
-    α =atan.(xy[:,2], xy[:,1]) 
 
+    thickness = b/nshells
+    c = [b*i/nshells for i in 0:nshells-1]
+    add(x) = x+thickness
+    lims = hcat(c, map(add,c))
+
+    nums = zeros(size(lims,1))
+    areas = zeros(size(lims,1))
+    i = 1
+    for row in eachrow(lims) 
+        r1 = row[1]
+        r2 = row[2]
     
-    a = L/2 #These are the proportions used in the main code
-    b = L/4
-    rₑ = (a-R)*(b-R)./(sqrt.((((a-R)*sin.(α)).^2) .+ ((b-R)*cos.((α))).^2))
-    nums = zeros(nshells)
+        idx = (rₚ .>= r1  .&& rₚ.<= r2)
 
-    for i in 1:nshells
-        id = rₚ .> lims[i]
-        nums[i] = sum(id)
+        nums[i] = sum(idx)
+        areas[i] = intersection_area(a,b,r1,r2)
+        i+=1 # smarter way to do that?
     end
-    area = π*((lims.+thickness).^2 .- lims.^2)
-    nc = [nums[i]-nums[i+1] for i in 1:length(nums)-1]
-    nc = vcat(nc, nums[end])
 
-    return nc
+    return nums./areas
 end
 
+Random.seed!(666)
+## MC checking
+a,b = (1.,0.5)
+N = 100
+xy = (rand(N,2).-0.5).*repeat(2 .*[a b],N)
+
+r = (xy[:,1]).*(xy[:,1]) + (xy[:,2]).*(xy[:,2])
+rₚ = sqrt.(r)   
+α =atan.(xy[:,2], xy[:,1]) 
+
+rₑ = (a)*(b)./(sqrt.((((a)*sin.(α)).^2) .+ ((b)*cos.((α))).^2))
+
+id = (rₚ .< (rₑ))
+xy1 = [xy[id,1] xy[id,2]]
+
+radial_density_el(xy1,10,a,b)
 
 
+
+#=
 # rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
 function circle(x,y,r)
     θ = 0:1:360
     Plots.Shape(r*sind.(θ) .+ x, r*cosd.(θ) .+ y)
 end
+
+
 
 
 
@@ -89,3 +150,4 @@ for l in 1:5
 plot!(circle(0,0,l), opacity=.2)
 end
 p
+=#
--- test ellipsector.jl
+++ test ellipsector.jl
@@ -3,7 +3,7 @@ using LinearAlgebra
 using Plots
 using Random
 
-function intersection_area(a,b,R₁,R₂) #a larger semiaxis, b smaller semiaxis, R₁ smaller circle radius
+function intersection_area_el(a,b,R₁,R₂) #a larger semiaxis, b smaller semiaxis, R₁ smaller circle radius
 
     if R₂ <= b
         area = pi*(R₂^2-R₁^2)
@@ -38,52 +38,73 @@ end
 # println(ellipse_sector_area(1,0.5,0.4,0.2))
 # println(ellipse_sector(1,0.5,0.6))
 
+function intersection_area_sq(r::Float64, L::Float64)
+    l = L/2
 
+    if r<=l 
+        A = pi*r^2
+    else
+        x = sqrt(r^2-l^2)
+        y = l 
+        θ = atan(y,x)-atan(x,y)
 
-## MC checking
-a,b = (1,0.5)
-N = 10000
-xy = (rand(N,2).-0.5).*repeat(2 .*[a b],N)
+        A = 4*x*l + 2*θ*r^2
+    end
 
-r = (xy[:,1]).*(xy[:,1]) + (xy[:,2]).*(xy[:,2])
-rₚ = sqrt.(r)   
-α =atan.(xy[:,2], xy[:,1]) 
-
-rₑ = (a)*(b)./(sqrt.((((a)*sin.(α)).^2) .+ ((b)*cos.((α))).^2))
-
-id = (rₚ .< (rₑ))
-xy1 = [xy[id,1] xy[id,2]]
-
-rₜ = sqrt.((xy1[:,1]).*(xy1[:,1]) + (xy1[:,2]).*(xy1[:,2]))
-
-# idx = (rₜ .>= 0.6 .&& rₜ .< 0.7)
-# xy2 = [xy1[idx,1] xy1[idx,2]]
-# print(size(xy2,1))
-# p = scatter(xy1[:,1], xy1[:,2], legend = false, aspect_ratio = :equal)
-# p = scatter!(xy2[:,1], xy2[:,2], legend = false, aspect_ratio = :equal)
-
-# p
-
-
-
-rv = [[0.3, 0.4] [0.4, 0.6] [0.6, 0.7]]
-for i in eachcol(rv)
-    r1 = i[1]
-    r2 = i[2]
-
-    idx = (rₜ .>= r1  .&& rₜ.<= r2)
-
-    xy2 = [xy1[idx,1] xy1[idx,2]]
-
-
-    el_area = pi*a*b
-    c = round(intersection_area(a,b,r1,r2)/el_area, digits = 5)
-    d = round(size(xy2, 1)/size(xy1,1),digits = 5)
-
-    println("$c,$d,$(size(xy2,1))")
+    return A
 end
 
+intersection_area_sq(0.9,1.)
+# ## MC checking
+# a,b = (1,0.5)
+# N = 10000
+# xy = (rand(N,2).-0.5).*repeat(2 .*[a b],N)
+
+# r = (xy[:,1]).*(xy[:,1]) + (xy[:,2]).*(xy[:,2])
+# rₚ = sqrt.(r)   
+# α =atan.(xy[:,2], xy[:,1]) 
+
+# rₑ = (a)*(b)./(sqrt.((((a)*sin.(α)).^2) .+ ((b)*cos.((α))).^2))
+
+# id = (rₚ .< (rₑ))
+# xy1 = [xy[id,1] xy[id,2]]
+
+# rₜ = sqrt.((xy1[:,1]).*(xy1[:,1]) + (xy1[:,2]).*(xy1[:,2]))
+
+# # idx = (rₜ .>= 0.6 .&& rₜ .< 0.7)
+# # xy2 = [xy1[idx,1] xy1[idx,2]]
+# # print(size(xy2,1))
+# # p = scatter(xy1[:,1], xy1[:,2], legend = false, aspect_ratio = :equal)
+# # p = scatter!(xy2[:,1], xy2[:,2], legend = false, aspect_ratio = :equal)
+
+# # p
+
+
+
+# rv = [[0.3, 0.4] [0.4, 0.6] [0.6, 0.7]]
+# for i in eachcol(rv)
+#     r1 = i[1]
+#     r2 = i[2]
+
+#     idx = (rₜ .>= r1  .&& rₜ.<= r2)
+
+#     xy2 = [xy1[idx,1] xy1[idx,2]]
+
+
+#     el_area = pi*a*b
+#     c = round(intersection_area_el(a,b,r1,r2)/el_area, digits = 5)
+#     d = round(size(xy2, 1)/size(xy1,1),digits = 5)
+
+#     println("$c,$d,$(size(xy2,1))")
+# end
 
 # p = scatter(xy1[:,1], xy1[:,2], legend = false, aspect_ratio = :equal)
 # p = scatter!(xy2[:,1], xy2[:,2], legend = false, aspect_ratio = :equal)
 # p
+# R = 1
+# nshells = 5
+# thickness = R/nshells
+# add(x) = x+thickness
+# lims = [i*thickness for i in 0:nshells-1]
+# lims = hcat(lims, map(add, lims))
+# lims[:,1].*lims[:,2]
