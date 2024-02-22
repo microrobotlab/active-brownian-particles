9e9faefcdd7a3cba7f8738f48d97dbad012e8d2c
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2024-02-22 16:30:46 +0100

    plotting of curvature-final

--- calculations.jl
+++ calculations.jl
@@ -1,10 +1,11 @@
 # this is a rough code for any calculations
 # not to be taken seriously
-using CalculusWithJulia, Plots
+using CalculusWithJulia, Plots, Unitful
 L=100
 a= L/2
 b= L/4
-
+path="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2024\\"
+f= path*"curvature.png"
 θ1= [ θ for θ in -π:0.01:π]  #in radian
 
 angle= [atan(b/a)] # angle at which the pole area= equator area 
@@ -14,19 +15,11 @@ angle= [atan(b/a)] # angle at which the pole area= equator area
 
 κ_angle= [a*b./((a*a*sin.(θ)*sin.(θ))+(b*b*cos.(θ)*cos.(θ)))^(3/2) for θ in angle]
 
+kx= [a*cos.(θ) for θ in -π:0.01:π]
 
+ky= [b*sin.(θ) for θ in -π:0.01:π]
 
-j= plot(L/2*cos.(-π:0.01:π), L/4*sin.(-π:0.01:π),marker_z=κ , color=:rainbow,aspect_ratio=:equal,legend=true) 
+k=plot(kx,ky, marker_z= κ, color=:lajolla, seriestype=:scatter, label="test",aspect_ratio=:equal, xlabel="x[μm]", ylabel="y[μm]")
+display(k)
+savefig(k,f)
 
-
-
-#k=scatter(θ1, κ, marker_z= κ, color=:rainbow,legend=true)
-display(j)
-
-#display(scatter(θ1,κ))
-
-#display(scatter!(angle,κ_angle))
-#k= 8*a*b/((3*a*a+b*b)^(3/2))
-
-
-k1= 8*8/(L*((13)^(3/2)))
