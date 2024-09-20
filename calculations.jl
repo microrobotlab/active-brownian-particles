07dd2b7f004281de48a596c4aa9ab274327e6c36
spm-sudo-echo <offical.jyoti.physics@gmail.com>
2024-09-20 11:59:54 +0200

    two particles inside a circle..julia updates

--- calculations.jl
+++ calculations.jl
@@ -1,11 +1,11 @@
-# this is a rough code for any calculations
-# not to be taken seriously
+# this is a code for curvature calculations
+
 using CalculusWithJulia, Plots, Unitful
 L=100
 a= L/2
 b= L/4
 path="C:\\Users\\j.sharma\\OneDrive - Scuola Superiore Sant'Anna\\P07 Coding\\2024\\"
-f= path*"curvature.png"
+f= path*"curvature1.png"
 θ1= [ θ for θ in -π:0.01:π]  #in radian
 
 angle= [atan(b/a)] # angle at which the pole area= equator area 
@@ -19,7 +19,8 @@ kx= [a*cos.(θ) for θ in -π:0.01:π]
 
 ky= [b*sin.(θ) for θ in -π:0.01:π]
 
-k=plot(kx,ky, marker_z= κ, color=:lajolla, seriestype=:scatter, label="test",aspect_ratio=:equal, xlabel="x[μm]", ylabel="y[μm]")
+k=plot(kx,ky, marker_z= κ,color = :lajolla,seriestype=:scatter,label="",aspect_ratio=:equal, xlabel="x[μm]", ylabel="y[μm]")
 display(k)
 savefig(k,f)
 
+#lajolla
