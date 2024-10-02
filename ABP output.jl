52f71437754daa46b29c8b47275b3ce52c004cdd
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-10-02 14:50:34 +0200

    minor

--- ABP output.jl
+++ ABP output.jl
@@ -12,7 +12,7 @@ include("ABP radialdensity.jl")
 using CSV, DataFrames, Dates, Distances, Distributions, Logging, NaNStatistics, Plots, Printf, Random
 gr()
 
-N = 500000
+N = 1000
 Delta_t = 1e-3
 t_tot= N*Delta_t
 
@@ -32,7 +32,7 @@ ICS=1      # number of intial conditons to be scanned
 #pf_factor = (R^2)/(a*b)
 pf_factor = (R^2)
 DT, DR, γ = diffusion_coeff(R).*[1e12, 1, 1]  #1
-packing_fraction = 0.01 #Largest pf for spherical beads π/4 = 0.7853981633974483
+packing_fraction = 0.1 #Largest pf for spherical beads π/4 = 0.7853981633974483
 
 area_el = π*a*b
 Np = round(Int,packing_fraction*L^2/(pi*R^2))
@@ -52,7 +52,7 @@ mainfolder= mkdir(path*"$datestamp")    # creates a folder names todays'late
 
 path1= path*"$datestamp\\"
 
-mainname = @sprintf "R=%.1f v=%.1f a=%.1f b=%.1f pf=%.4f dt=%.0e Nt=%.0e" R v a b packing_fraction Delta_t Nt
+mainname = @sprintf "R=%.1f v=%.1f a=%.1f b=%.1f pf=%.4f dt=%.0e Nt=%.1e" R v a b packing_fraction Delta_t Nt
 
 mainfolder1= mkdir(path1*mainname)
 
@@ -91,7 +91,7 @@ for i=1:ICS
     # THIS IS THE CODE TO CALL WALL FUNCTIONS IN THE MAIN FUNCTION for elliptical wall condition
 
     start_sim = now()
-    graph_wall = multiparticleE(Np,L,R,Uniform(0.,10.),Normal(0.,0.025),Nt,Delta_t) # has values of x and y position in each frame in graph_wall[1]
+    graph_wall = multiparticleE(Np,L,R,Exponential(12/log(50)),Normal(0.,0.025),Nt,Delta_t) # has values of x and y position in each frame in graph_wall[1]
     elapsed_time = Dates.canonicalize(now()-start_sim)
     #sim_time = @elapsed graph_wall = multiparticleE(Np,L,R,v,Nt) # has values of x and y position in each frame in graph_wall[1]
 
@@ -101,7 +101,7 @@ for i=1:ICS
     file_store(graph_wall,Nt,pathf)
     #-------------------------------------------------------------------------------------------------------------------------------------------------------------
     # analysis
-    inside_Np=stat_analysis1(a,b,R,pathf)
+    # inside_Np=stat_analysis1(a,b,R,pathf)
     # mean and standard deviation
 
     #analysis_SD= stat_analysis2(a,b,R,pathf)
