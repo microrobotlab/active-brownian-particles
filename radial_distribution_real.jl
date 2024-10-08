3f312384c8699e282d44bc575a042a872495820d
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-10-08 16:32:35 +0200

    minor

--- radial_distribution_real.jl
+++ radial_distribution_real.jl
@@ -1,60 +1,25 @@
-using CSV, DataFrames, Distances, FFTW, LinearAlgebra, Plots, ProgressBars, Random, Statistics
-include("geo_toolbox.jl")
+using CSV, DataFrames, FFTW, JSON3, LinearAlgebra, Plots, Random, Statistics
 
-function periodic_BC_array!(xy::Array{Float64,2},L::Float64, R)   #when a particle crosses an edge it reappears on the opposite side
-	# Boundary conditions: horizontal edge
-	idx = abs.(xy[:,1]) .> L/2 + R #I create vector idx in which I have 1 where the absolute value of the x coordinate of the particles is outside the observation area
-	if any(idx)
-		xy[idx,1] .-= sign.(xy[idx,1]).*L   #where I have uni in idx I make the particle reappear on the opposite side of x with respect to 0
-	end
-	# Boundary conditions: vertical edge
-	idy = abs.(xy[:,2]) .> L/2 + R
-	if any(idy)
-		xy[idy,2] .-= sign.(xy[idy,2]).*L
-	end
-	return nothing
+rdf = CSV.read("..\\simulations\\20241008-160813\\data\\run1\\20241008-160813_run1_rdf.csv", DataFrame)
+
+rdf.RadialDistributionFunction = [JSON3.read(x) for x in rdf.RadialDistributionFunction]
+begin
+    lendf = size(rdf)[1]
+    div = Int(lendf//groupsize)
+    grouplabel = [(i÷div) for i in 0:lendf-1]
+    rdf[!,:GroupLabel] = grouplabel
+
+    rdf_avg = transform(groupby(rdf,:GroupLabel), :RadialDistributionFunction => ((x)->(mean(x),)) => :RDF_mean)
+    transform!(rdf_avg, :RDF_mean => ByRow(x -> x[1]) => :RDF_mean)
 end
 
-rdfp = plot()
-p2 = plot()
-
-L = 300.
-R = 2.
-maxdist = L*sqrt(2.)/2
-nbins = 1000
-rs = LinRange(maxdist/nbins,maxdist,nbins)
-areas = [intersection_area_sq(r,L) for r in rs]
-
-bins = areas-circshift(areas,1)
-bins[1] = areas[1]
-
-for fname in ["range5sigma2epsilon0.5.csv"]# "range5sigma2epsilon0.1.csv"]# "range5sigma2epsilon1.csv" ]
-    df = CSV.read(fname, DataFrame)
-    Np = maximum(unique(df[!,:N]))
-    dens = Np/L^2
-    bins_ensemble = Matrix{Int}(undef, nbins,0)
-    for time in unique(df[end,:Time])
-        inst_df = filter(:Time => t->t==time, df)
-        xy = Matrix(inst_df[!, [:xpos, :ypos]])
-        for xyc in eachrow(xy)
-            xy_shifted = xy.-xyc'
-            periodic_BC_array!(xy_shifted, L, R)
-            r = sqrt.((xy_shifted[:,1]).*(xy_shifted[:,1]) + (xy_shifted[:,2]).*(xy_shifted[:,2]))
-            cumulative = [sum(r[r.!=0].<rb) for rb in rs]
-            parts_in_bins = cumulative-circshift(cumulative,1)
-            parts_in_bins[1] = cumulative[1]
-            bins_ensemble = hcat(bins_ensemble, parts_in_bins)
-        end
+begin
+    p = plot()
+    start,end_ = 0,20
+    xticks!(p, start:1:end_)
+    xlims!(p,start,end_)
+    for i in unique(10 .* grouplabel .+1)
+        plot!(p, rs, rdf_avg[i,:RDF_mean])
     end
-    rdf = mean(bins_ensemble, dims = 2)./(bins*dens)
-    pos_id = vec(rdf.>0)
-    # plot!(rdfp, rs[pos_id], rdf[pos_id])
-    plot!(rdfp, rs, rdf)
-    k = fftshift(fftfreq(length(rs), 1/rs[1]))
-    ssf = 1 .+ dens.*abs.(fftshift(fft(rdf)))
-    plot!(p2, k[k.>0], ssf[k.>0])#, xaxis=:log, yaxis=:log)
 end
-# yaxis!(rdfp, :log)
-xlims!(rdfp,0,20)
-display(rdfp)
-display(p2)
+display(p)
