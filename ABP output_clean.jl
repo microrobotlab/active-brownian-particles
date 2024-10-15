209354bcb9f3dcc98c2c45a4f89e18319d67b34c
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-10-15 10:40:35 +0200

    Bad version

--- ABP output_clean.jl
+++ ABP output_clean.jl
@@ -15,23 +15,23 @@ gr()
 
 ## USER INTERFACE
 # destination folders selection
-path="C:\\Users\\nikko\\OneDrive\\Documents\\Uni\\magistrale\\tesi\\simulations\\" # destination directory path
+path="C:\\Users\\picch\\abp_simulations\\simulations" # destination directory path
 
 ## PARAMETERS SET
 # Simulation parameters
-Nt = 100000             # number of steps
+Nt = 2000000             # number of steps
 Delta_t = 1e-3          # s step time
 ICS=1                   # Number of intial conditons to be scanned 
-animation_ds = 100     # Downsampling in animation
+animation_ds = 500     # Downsampling in animation
 file_ds = 100           # Downsampling in file
 
 
 # Physical parameters
 BC_type = :periodic    # :periodic or :wall
 box_shape = :square    # shapes: :square, :circle, :ellipse
-L = 300.0 	           # μm box length
+L = 100.0 	           # μm box length
 R = 2.0		           # μm particle radius
-packing_fraction = 0.05 # Largest pf for spherical beads π/4 = 0.7853981633974483
+packing_fraction = 0.1 # Largest pf for spherical beads π/4 = 0.7853981633974483
 # Velocities can also be distributions e.g. v = Normal(0.,0.025)
 v = Exponential(12/log(50)) 	           # μm/s particle s
 ω = Normal(0.,0.025)                # s⁻¹ particle angular velocity
--- radial_distribution_real.jl
+++ radial_distribution_real.jl
@@ -1,12 +1,13 @@
 using CSV, DataFrames, FFTW, FindPeaks1D, JSON3, LinearAlgebra, Plots, Random, Statistics
+include("ABP radialdistribution.jl")
 
-rdf = CSV.read("..\\simulations\\20241009-132045\\data\\run1\\20241009-132045_run1_rdf.csv", DataFrame)
-bindata = CSV.read("..\\simulations\\20241009-132045\\data\\run1\\20241009-132045_run1_rdfbin.csv", DataFrame)
+rdf = CSV.read("..\\simulations\\20241014-094849\\data\\run1\\20241014-094849_run1_rdf.csv", DataFrame)
+bindata = CSV.read("..\\simulations\\20241014-094849\\data\\run1\\20241014-094849_run1_rdfbin.csv", DataFrame)
 rs = bindata[!,:Radius]
 
 rdf.RadialDistributionFunction = [JSON3.read(x) for x in rdf.RadialDistributionFunction]
 begin
-    ngroups = 250
+    ngroups = 2000
     lendf = size(rdf)[1]
     div = Int(lendf//ngroups)
     grouplabel = [(i÷div) for i in 0:lendf-1]
@@ -23,17 +24,18 @@ begin
     xlims!(p,start,end_)
     times = Int[]
     peaks = Float64[]
-    for i in unique(div .* grouplabel .+1)[1:25:end]
+    for i in unique(div .* grouplabel .+1)
         averaged_rdf = rdf_avg[i,:RDF_mean]
         pkindices, properties = findpeaks1d(averaged_rdf;
-                                            height = 2., 
-                                            prominence = 0.4,)
-        push!(peaks, properties["peak_heights"][1])
+                                            height = 5., 
+                                            prominence = 0.5,)
+        push!(peaks, maximum(properties["peak_heights"]))
         push!(times, rdf_avg[i,:Time])
         plot!(p, rs, averaged_rdf)
         scatter!(p, rs[pkindices], averaged_rdf[pkindices])
     end
     p2 = plot()
-    scatter(p2, times, peaks)
+    plot!(p2, times, peaks)
 end
 display(p)
+display(p2)
