fd1c93f05ef9dc476ba3b57da258aa91ab9bef9d
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-10-30 11:53:57 +0100

    added parquet

--- ABP_file.jl
+++ ABP_file.jl
@@ -4,7 +4,7 @@
 # INPUT: array of postions and orientation
 # OUTPUT: saved excel file (f1)
 
-using CSV, DataFrames, DelimitedFiles, Markdown
+using CSV, DataFrames, DelimitedFiles, Markdown, Parquet
 
 """
     file_store_csv(graph_wall,Nt,pathf;downsampling = 100)
@@ -83,6 +83,43 @@ function file_store_txt(graph_wall,Nt,pathf;downsampling::Int=100)
 
     CSV.write(f1, data)
     
+    @info "$(now()) Out of ABP file"
+    return nothing
+end
+
+function file_store_parquet(graph_wall,Nt,pathf;downsampling::Int=100)
+    f1= pathf*".parquet"               # destination file name
+
+    pnumber=[]
+    time=[]
+    x=[]
+    y=[]
+    θ=[]
+    for i = 1:downsampling:Nt+1
+        for j = 1:length(graph_wall[1][i][:,1])
+            push!(pnumber,j)
+            push!(time, i)
+        push!(x, graph_wall[1][i][j,1])
+        push!(y, graph_wall[1][i][j,2])
+        push!(θ, graph_wall[2][i,1][j])
+        end
+    end
+
+    touch(f1)
+
+    efg = open(f1, "w")
+    
+    #creating DataFrame
+    data = DataFrame(
+    N= pnumber,
+    Time= time,
+    xpos= x,
+    ypos= y,
+    orientation=θ) 
+
+    Parquet.write_parquet(f1, data)
+    
+    close(efg)
     @info "$(now()) Out of ABP file"
     return nothing
 end
