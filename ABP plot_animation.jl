f48229a51fc8bee8d75578a115cb63c8d20d37ef
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-11-25 12:11:07 +0100

    minor

--- ABP plot_animation.jl
+++ ABP plot_animation.jl
@@ -1,10 +1,10 @@
 using GLMakie
-using GeometryBasics: Point2f, Circle, Point2f0
-using CSV, DataFrames, DelimitedFiles,LinearAlgebra, Markdown, Parquet
+using GeometryBasics: Point2f, Circle, Point2f0, Polygon
+using CSV, DataFrames, Dates, Logging
 
-function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Float64, downsampling::Int, ext::String=".txt"; record::Bool=false, final_format::String="gif")
+function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Float64, measevery::Int, downsampling::Int=1; ext::String=".txt", record::Bool=false, final_format::String="gif")
     fname = pathf*ext
-    timestep *= downsampling
+    timestep *= measevery*downsampling
     df = CSV.read(fname, DataFrame)    
     Np = maximum(df[!,:N])
     xpos = Array(df[!,:xpos])
@@ -29,7 +29,7 @@ function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Fl
     sc = GLMakie.scatter!(ax, xs, ys, marker = Polygon(mrk),markersize = 200/L)
     ar = GLMakie.arrows!(ax, xs, ys, us, vs, color = :black, lengthscale=R, arrowsize = 300R/L)
 
-    timestamps = 1:maximum(df[!,:Time])÷downsampling
+    timestamps = 1:downsampling:(maximum(df[!,:Time])÷downsampling)
 
     slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
 
@@ -42,16 +42,17 @@ function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Fl
         framerate = Int(1/timestep)) do t
             simstep[] = t
         end
+        @info "$(now()) Animation saved to $pathf.$final_format"
     end
     return fig
 
 end
 
-function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int, timestep::Float64,Nt::Int,downsampling::Int; record::Bool=false)
+function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int, timestep::Float64,Nt::Int,measevery::Int, downsampling::Int=1; record::Bool=false, final_format::String="gif")
     pos = vcat(graph_wall[1]...)
     orient = vcat(graph_wall[2]...)
 
-    timestep *= downsampling
+    timestep *= measevery*downsampling
 
     xpos = reshape(pos[:,1], Np,:)[:,1:downsampling:end]
     ypos = reshape(pos[:,2], Np,:)[:,1:downsampling:end]
@@ -59,7 +60,6 @@ function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int
     u,v = cos.(θ), sin.(θ)
 
     simstep = Observable(1)
-
     xs = @lift xpos[:,$simstep]
     ys = @lift ypos[:,$simstep]
     us = @lift u[:,$simstep]
@@ -71,7 +71,7 @@ function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int
     sc = GLMakie.scatter!(ax, xs, ys, marker = Polygon(mrk),markersize = 200/L)
     ar = GLMakie.arrows!(ax, xs, ys, us, vs, color = :black, lengthscale=R, arrowsize = 300R/L)
 
-    timestamps = 1:Nt÷downsampling
+    timestamps = 1:downsampling:(Nt÷downsampling)
 
     slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
 
@@ -84,6 +84,7 @@ function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int
         framerate = Int(1/timestep)) do t
             simstep[] = t
         end
+        @info "$(now()) Animation saved to $pathf.$final_format"
     end
     return fig
 
