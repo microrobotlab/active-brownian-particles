0c87c16e12c143d75ae2c89b73d0587f1c638f6a
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-11-27 14:54:50 +0100

    added show

--- ABP plot_animation.jl
+++ ABP plot_animation.jl
@@ -2,7 +2,7 @@ using GLMakie
 using GeometryBasics: Point2f, Circle, Point2f0, Polygon
 using CSV, DataFrames, Dates, Logging
 
-function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Float64, measevery::Int, downsampling::Int=1; ext::String=".txt", record::Bool=false, final_format::String="gif")
+function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Float64, measevery::Int, downsampling::Int=1; ext::String=".txt", show::Bool=true, record::Bool=false, final_format::String="gif")
     fname = pathf*ext
     timestep *= measevery*downsampling
     df = CSV.read(fname, DataFrame)    
@@ -29,28 +29,30 @@ function animation_from_file(pathf::String, L::Float64, R::Float64, timestep::Fl
     sc = GLMakie.scatter!(ax, xs, ys, marker = Polygon(mrk),markersize = 200/L)
     ar = GLMakie.arrows!(ax, xs, ys, us, vs, color = :black, lengthscale=R, arrowsize = 300R/L)
 
-    timestamps = 1:downsampling:(maximum(df[!,:Time]))
-
-    slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
-
-    on(slider.value) do val
-        simstep[] = round(Int, val)
-    end
-    
     if record
+        timestamps = 1:downsampling:(maximum(df[!,:Time]))
         GLMakie.record(fig, pathf*".$final_format", timestamps;
         framerate = Int(1/timestep)) do t
             simstep[] = t
         end
         @info "$(now()) Animation saved to $pathf.$final_format"
     end
-    return fig
+
+    if show
+        slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
+
+        on(slider.value) do val
+            simstep[] = round(Int, val)
+        end
+        display(fig)
+    end
+    return nothing
 
 end
 
-function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int, timestep::Float64,Nt::Int,measevery::Int, downsampling::Int=1; record::Bool=false, final_format::String="gif")
-    pos = vcat(graph_wall[1]...)
-    orient = vcat(graph_wall[2]...)
+function animation_from_history(history, pathf, L::Float64, R::Float64, Np::Int, timestep::Float64,Nt::Int,measevery::Int, downsampling::Int=1; show::Bool=true, record::Bool=false, final_format::String="gif")
+    pos = vcat(history[1]...)
+    orient = vcat(history[2]...)
 
     timestep *= measevery*downsampling
 
@@ -71,21 +73,23 @@ function animation_from_graph(graph_wall, pathf, L::Float64, R::Float64, Np::Int
     sc = GLMakie.scatter!(ax, xs, ys, marker = Polygon(mrk),markersize = 200/L)
     ar = GLMakie.arrows!(ax, xs, ys, us, vs, color = :black, lengthscale=R, arrowsize = 300R/L)
 
-    timestamps = 1:downsampling:(Nt)
-
-    slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
-
-    on(slider.value) do val
-        simstep[] = round(Int, val)
-    end
-    
     if record
+        timestamps = 1:downsampling:(Nt)
         GLMakie.record(fig, pathf*".$final_format", timestamps;
         framerate = Int(1/timestep)) do t
             simstep[] = t
         end
         @info "$(now()) Animation saved to $pathf.$final_format"
     end
-    return fig
+
+    if show
+        slider = GLMakie.Slider(fig[2, 1], range=1:size(xpos, 2), startvalue=1)
+
+        on(slider.value) do val
+            simstep[] = round(Int, val)
+        end
+        display(fig)
+    end
+    return nothing
 
 end
