89f462123de895470109208fb14592b825fcd9e3
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-12-03 16:24:15 +0100

    skeleton

--- ABP output_update.jl
+++ ABP output_update.jl
@@ -10,4 +10,15 @@ include("ABP multifolder.jl")
 # include("ABP radialdensity.jl")
 include("ABP plot_animation.jl")
 # include("ABP average.jl")
-using CSV, DataFrames, Dates, Distances, Distributions, JLD2, Logging, NaNStatistics, Printf, Random
+using ArgParse, CSV, DataFrames, Dates, Distances, Distributions, JLD2, Logging, NaNStatistics, Printf, Random
+
+function main()
+    ABPE = ABPE2
+    ABPE, matrices = initABPE( Np, L, R, T, v, ω, int_func, forward, offcenter, range, int_params...,)
+    for iter in 0:Nt
+        update!(ABPE,matrices,δt, forward, offcenter, range, int_func, int_params...)
+        if iter % measevery == 0
+            
+        end
+    end
+end
