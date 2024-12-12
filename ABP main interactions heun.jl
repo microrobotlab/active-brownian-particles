2b82f9120a02804edd1bf7390e6acd4da44d99d3
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-12-12 17:00:21 +0100

    Heun implemented

--- ABP main interactions heun.jl
+++ ABP main interactions heun.jl
@@ -33,15 +33,7 @@ function initABPE(Np::Int64, L::Float64, R::Float64, T::Float64, vd::Union{Float
     xyθ[:,1:2], dists, superpose, uptriang = hardsphere_periodic(xyθ[:,1:2], R, L) #xyθ[:,1:2] gives x and y positions of intitial particles
     v = rand(vd, Np)
     ω = rand(ωd,Np)
-    if (!isapprox(offcenter,0.0))
-        force, torque = force_torque(xyθ[:,1:2], xyθ[:,3], R, L, forward, offcenter, range, int_func, int_params...)
-    else
-        force = interactions_range(xyθ[:,1:2], R, L, range, Np, int_func, int_params...)
-        torque = zeros(Np)
-    end
-    fx = force[:,1]
-    fy = force[:,2]
-    abpe = ABPE2( Np, L, R, T, v, ω, 1e12DT, DR, xyθ[:,1], xyθ[:,2], xyθ[:,3], fx, fy, torque)
+    abpe = ABPE2( Np, L, R, T, v, ω, 1e12DT, DR, xyθ[:,1], xyθ[:,2], xyθ[:,3])
     return abpe, (dists, superpose, uptriang)
 end
 
@@ -99,21 +91,26 @@ torque(abpe::ABPE2) = abpe.torque
 
 function update_heun(abpe::ABPE, matrices::Tuple{Matrix{Float64}, BitMatrix, BitMatrix}, δt::Float64, forward::Bool, offcenter::Float64, range::Float64, int_func::Function, int_params...) where {ABPE <: ABPsEnsemble}
 
-    δp = Array{Float64,2}(undef,abpe.Np,2)
-    δθ = Array{Float64,1}(undef,abpe.Np)
+    δp_i = Array{Float64,2}(undef,abpe.Np,2)
+    δθ_i = Array{Float64,1}(undef,abpe.Np)
+    δp_f = Array{Float64,2}(undef,abpe.Np,2)
+    δθ_f = Array{Float64,1}(undef,abpe.Np)
     γₜ = diffusion_coeff(1e-6*abpe.R, abpe.T)[3] #Output in international system units kg/s
     γᵣ = (8e-12γₜ / 6) * abpe.R^2                #Output in international system units
 
     #intermediate step
     if (!isapprox(offcenter,0.0))
-        f_i, t_i = force_torque(pθ[1], pθ[2], abpe.R, abpe.L, forward, offcenter, range, int_func, int_params...)
+        f_i, t_i = force_torque(position(abpe), orientation(abpe), abpe.R, abpe.L, forward, offcenter, range, int_func, int_params...)
+        # f_i .+= interactions_range(position(abpe), abpe.R, abpe.L, 2R-1e-3, abpe.Np, excluded_volume, abpe.R, 1.)
     else
-        f_i = interactions_range(pθ[1], abpe.R, abpe.L, range, abpe.Np, int_func, int_params...)
+        f_i = interactions_range(position(abpe), abpe.R, abpe.L, range, abpe.Np, int_func, int_params...)
+        # f_i .+= interactions_range(position(abpe), abpe.R, abpe.L, 2R-1e-3, abpe.Np, excluded_volume, abpe.R, 1.)
         t_i = zeros(abpe.Np)
     end 
 
-    δp_i .= sqrt.(2*δt*abpe.DT)*randn(abpe.Np,2) .+ δt.*abpe.v.*[cos.(abpe.θ) sin.(abpe.θ)] .+ δt*1e-6f_i/γₜ 
-    δθ_i .= sqrt(2*abpe.DR*δt)*randn(abpe.Np) .+ δt.*abpe.ω .+ δt*1e-18t_i/γᵣ
+    det_part_i = (abpe.v.*[cos.(abpe.θ) sin.(abpe.θ)] .+ 1e-6f_i/γₜ, abpe.ω .+ 1e-18t_i/γᵣ)
+    δp_i .= sqrt.(2*δt*abpe.DT)*randn(abpe.Np,2) .+ δt.*det_part_i[1]
+    δθ_i .= sqrt(2*abpe.DR*δt)*randn(abpe.Np) .+ δt.*det_part_i[2]
 
     pθ_i = (position(abpe), orientation(abpe)) .+ (δp_i, δθ_i)
 
@@ -123,15 +120,18 @@ function update_heun(abpe::ABPE, matrices::Tuple{Matrix{Float64}, BitMatrix, Bit
     #final step
     if (!isapprox(offcenter,0.0))
         f_f, t_f = force_torque(pθ_i..., abpe.R, abpe.L, forward, offcenter, range, int_func, int_params...)
+        # f_f .+= interactions_range(pθ_i[1], abpe.R, abpe.L, 2R-1e-3, abpe.Np, excluded_volume, abpe.R, 1.)
     else
-        f_f = interactions_range(pθ[1], abpe.R, abpe.L, range, abpe.Np, int_func, int_params...)
+        f_f = interactions_range(pθ_i[1], abpe.R, abpe.L, range, abpe.Np, int_func, int_params...)
+        # f_f .+= interactions_range(pθ_i[1], abpe.R, abpe.L, 2R-1e-3, abpe.Np, excluded_volume, abpe.R, 1.)
         t_f = zeros(abpe.Np)
     end 
 
-    δp .= sqrt.(2*δt*abpe.DT)*randn(abpe.Np,2) .+ δt.*abpe.v.*[cos.(abpe.θ) sin.(abpe.θ)] .+ δt*1e-6f_f/γₜ 
-    δθ .= sqrt(2*abpe.DR*δt)*randn(abpe.Np) .+ δt.*abpe.ω .+ δt*1e-18t_f/γᵣ
+    det_part_f = (abpe.v.*[cos.(pθ_i[2]) sin.(pθ_i[2])] .+ 1e-6f_f/γₜ, abpe.ω .+ 1e-18t_f/γᵣ)
+    δp_f .= sqrt.(2*δt*abpe.DT)*randn(abpe.Np,2) .+ δt.*(det_part_f[1] .+ det_part_i[1])/2
+    δθ_f .= sqrt(2*abpe.DR*δt)*randn(abpe.Np) .+ δt.*(det_part_f[2] + det_part_i[2])/2
 
-    pθ = (position(abpe), orientation(abpe)) .+ (δp, δθ)
+    pθ = (position(abpe), orientation(abpe)) .+ (δp_f, δθ_f)
 
     periodic_BC_array!(pθ[1],abpe.L, abpe.R)
     hardsphere_periodic!(pθ[1], matrices[1], matrices[2],abpe.R, abpe.L)
