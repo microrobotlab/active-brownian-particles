4ecdcb70c8250d6ce24f30c401635852326bfd08
npicchiarelli <picchiarelliniccolo@gmail.com>
2025-01-23 17:07:50 +0100

    minor

--- ABP check.jl
+++ ABP check.jl
@@ -1,13 +1,13 @@
 using CSV, DataFrames
 
-path="D:\\NiccoloP\\simulations\\spring"
+path="C:\\Users\\nikko\\OneDrive\\Documents\\Uni\\magistrale\\tesi\\simulations\\"
 
-datestamp = "20250117-092238"
+datestamp = "20250117-123605"
 
 df_list = []
 for p in readdir(joinpath(path,datestamp,"data"), join = true)
     if isdir(p)
-        num = p[findfirst("run", p)[end]+1:end]
+        num = p[91:end]
         df = CSV.read(joinpath(p, datestamp*"_run$num.txt"), DataFrame)
         push!(df_list, df)
     end
