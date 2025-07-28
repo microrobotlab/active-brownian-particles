1abd9bd0b5d454459a13f54e14eda703920284bb
npicchiarelli <picchiarelliniccolo@gmail.com>
2025-07-28 11:17:10 +0200

    correct include statements

--- voronoi/ABP output_update_voronheun.jl
+++ voronoi/ABP output_update_voronheun.jl
@@ -3,13 +3,13 @@
 #VARIABLES: Destination folder path and filename
 include("ABP main interactions voronheun.jl")
 # include("ABP main.jl")
-include("../active-brownian-particles/ABP_file.jl")
-include("../active-brownian-particles/ABP radialdistribution.jl")
+include("../ABP_file.jl")
+include("../ABP radialdistribution.jl")
 # include("ABP SD.jl")
-include("../active-brownian-particles/ABP multifolder.jl")
+include("../ABP multifolder.jl")
 # include("ABP radialdensity.jl")
-include("../active-brownian-particles/ABP plot_animation.jl")
-include("../active-brownian-particles/ABP orderparameters.jl")
+include("../ABP plot_animation.jl")
+include("../ABP orderparameters.jl")
 # include("ABP average.jl")
 using  CSV, DataFrames, Dates, Distributions, JLD2, Logging, Printf, Random
 
